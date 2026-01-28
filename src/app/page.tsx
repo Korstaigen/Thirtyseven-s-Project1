7ddd5e7b551b678b20d3958ffce756ad06aa34d9
Korstaigen <germanemanuel2005@gmail.com>
2026-01-28 22:38:01 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,21 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900 text-white relative">
 
-      {/* Top Right - Real User Badge */}
-      <div className="absolute top-4 right-4">
+      {/* Top Left GIF */}
+      <div className="absolute top-4 left-4 z-40">
+        <img
+          src="https://tenor.com/b1l6t.gif"
+          alt="guild gif"
+          className="w-[75px] h-[75px] rounded-lg shadow"
+        />
+      </div>
+
+      {/* Top Right User */}
+      <div className="absolute top-4 right-4 z-40">
         <UserBadge />
       </div>
 
-      {/* Centered Form */}
+      {/* Center Content */}
       <div className="flex items-center justify-center min-h-screen px-4">
         <GuildForm />
       </div>
