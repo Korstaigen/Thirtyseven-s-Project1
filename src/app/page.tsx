b372e79e203ec2e6516beba816341b956d17dca1
Korstaigen <germanemanuel2005@gmail.com>
2026-01-29 02:51:52 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import GuildForm from '@/components/GuildForm'
-import UserBadge from '@/components/UserBadge'
-import HardReserves from '@/components/HardReserves'
+import GuildForm from '@components/GuildForm'
+import UserBadge from '@components/UserBadge'
+import HardReserves from '@components/HardReserves'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
-
+    <main className="flex flex-wrap min-h-screen bg-gray-900 text-white relative overflow-hidden">
       {/* Top Left GIF */}
       <div className="absolute top-4 left-4 z-40">
         <img
-          src="https://media1.tenor.com/m/xgVPxE3ZsQAAAAAd/hop-on-turtlewow-turtlewow.gif"
+          src="https://media1.tenor.com/m/xgVPxE3zSQAAAAAd/hop-on-turtlewow-turtlewow.gif"
           alt="guild gif"
           className="w-[120px] h-[120px] rounded-lg shadow"
         />
@@ -21,15 +20,14 @@ export default function Home() {
       </div>
 
       {/* Hard Reserves (Right Side, Centered with Form) */}
-      <div className="absolute right-6 top-1/3 -translate-y-1/2 z-30 w-[320px] max-h-[70vh] overflow-y-auto">
+      <div className="absolute right-6 top-1/2 -translate-y-1/2 z-30 w-[320px] max-h-[70vh] overflow-y-auto">
         <HardReserves />
       </div>
 
       {/* Main Center Form */}
-      <div className="flex items-center justify-center min-h-screen px-4">
+      <div className="flex items-center justify-center min-h-screen px-6">
         <GuildForm />
       </div>
-
     </main>
   )
 }
