f59681191c690c7de6df513668c421de1f04f4d1
Korstaigen <germanemanuel2005@gmail.com>
2026-01-29 03:00:36 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import GuildForm from '@components/GuildForm'
-import UserBadge from '@components/UserBadge'
-import HardReserves from '@components/HardReserves'
+import GuildForm from '../components/GuildForm'
+import UserBadge from '../components/UserBadge'
+import HardReserves from '../components/HardReserves'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+
       {/* Top Left GIF */}
       <div className="absolute top-4 left-4 z-40">
         <img
@@ -28,7 +29,7 @@ export default function Home() {
       <div className="flex items-center justify-center min-h-screen px-4">
         <GuildForm />
       </div>
+
     </main>
   )
 }
- 
